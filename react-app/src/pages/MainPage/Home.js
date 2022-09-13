14236f88368480e6d0300ca3245768c8223aa784
Fu-taKagawa <kagahaya2@gmail.com>
2022-09-13 14:19:13 +0900

    change remote-url

--- react-app/src/pages/MainPage/Home.js
+++ react-app/src/pages/MainPage/Home.js
@@ -7,7 +7,9 @@ import { AuthContext } from '../../AuthService'
 
 const Home = () => {
     const [posts, setPosts] = useState([])
-    const [liked, setLiked] = useState(0)
+    const [liked, setLiked] = useState(false)
+    const noLike = document.getElementsByClassName("noLiking")
+    const Like = document.getElementsByClassName("Liking")
     const user = useContext(AuthContext)
     posts.sort((a, b) => a.timestamp - b.timestamp)
     useEffect(() => {
@@ -19,6 +21,17 @@ const Home = () => {
                 setPosts(posts)
             })
     }, [])
+    const handleLike = (e)=>{
+        e.preventDefault()
+        setLiked(!liked)
+        Like.classList.add("hidden")
+        console.log(liked)
+    }
+    const handleNoLike = (e)=>{
+        e.preventDefault()
+        setLiked(!liked)
+        Like.classList.add("")
+    }
     return (
         <>
             <Header/>
@@ -52,11 +65,16 @@ const Home = () => {
                                     <h1>{posts.title}</h1>
                                     <p>{posts.text}</p>
                                 </div>
+                                <div>
+                                    <button 
+                                        id='noLiking'
+                                        onClick={handleLike}>noLike
+                                    </button>
+                                </div>
                             </li>
                         ))
                     }
                 </ul>
-                
             </div>
         </>
     )
